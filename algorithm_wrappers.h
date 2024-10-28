ac1dfd5cf5eb05dc6e5b1fc9bbe4c2e255ebba6e
Kevin Leonardic <github@leonardic.de>
2024-10-28 14:08:06 +0100

    Use hwloc to determine number of physical cores thread_local storage for data

--- algorithm_wrappers.h
+++ algorithm_wrappers.h
@@ -34,70 +34,84 @@
 #pragma comment(lib, "bcrypt.lib")
 #endif
 
-struct sha256_dummy {
+struct sha256_dummy
+{
   void add_bytes(const unsigned char *, std::size_t) {}
   std::array<unsigned char, 32> digest() { return {}; }
 };
 
-struct sha256_zedwood {
+struct sha256_zedwood
+{
   zedwood::SHA256 ctx;
   sha256_zedwood() { ctx.init(); }
-  inline void add_bytes(const unsigned char *bytes, std::size_t num) {
+  inline void add_bytes(const unsigned char *bytes, std::size_t num)
+  {
     constexpr std::size_t max_bytes =
         (std::numeric_limits<unsigned int>::max)() / 2;
     unsigned int size = 0;
-    for (std::size_t i = 0; i < num; i += size, bytes += size) {
+    for (std::size_t i = 0; i < num; i += size, bytes += size)
+    {
       size = static_cast<unsigned int>((std::min)(max_bytes, num - i));
       ctx.update(bytes, size);
     }
   }
-  std::array<unsigned char, 32> digest() {
+  std::array<unsigned char, 32> digest()
+  {
     std::array<unsigned char, 32> tmp;
     ctx.final(tmp.data());
     return tmp;
   }
 };
 
-struct sha256_openssl_deprecated {
+struct sha256_openssl_deprecated
+{
   SHA256_CTX ctx = {};
   sha256_openssl_deprecated() { SHA256_Init(&ctx); }
-  void add_bytes(const unsigned char *bytes, std::size_t num) {
+  void add_bytes(const unsigned char *bytes, std::size_t num)
+  {
     static_assert((std::numeric_limits<size_t>::max)() ==
                       (std::numeric_limits<std::size_t>::max)(),
                   "Incompatible size_t");
     SHA256_Update(&ctx, bytes, num);
   }
-  std::array<unsigned char, 32> digest() {
+  std::array<unsigned char, 32> digest()
+  {
     std::array<unsigned char, 32> tmp;
     SHA256_Final(tmp.data(), &ctx);
     return tmp;
   }
 };
 
-struct openssl_evp_destroyer {
+struct openssl_evp_destroyer
+{
   void operator()(EVP_MD_CTX *ctx) const { EVP_MD_CTX_destroy(ctx); }
 };
 
-struct openssl_md_destroyer {
+struct openssl_md_destroyer
+{
   void operator()(EVP_MD *md) const { EVP_MD_free(md); }
 };
 
-struct sha256_openssl {
+struct sha256_openssl
+{
   std::unique_ptr<EVP_MD_CTX, openssl_evp_destroyer> ctx;
   const std::unique_ptr<EVP_MD, openssl_md_destroyer> md;
 
-  sha256_openssl() : ctx(EVP_MD_CTX_create()), md(EVP_MD_fetch(NULL, "SHA256", NULL)) {
+  sha256_openssl() : ctx(EVP_MD_CTX_create()), md(EVP_MD_fetch(NULL, "SHA256", NULL))
+  {
     assert(static_cast<bool>(ctx));
     EVP_DigestInit_ex(ctx.get(), md.get(), NULL);
   }
 
-  void add_bytes(const unsigned char *bytes, std::size_t num) {
+  void add_bytes(const unsigned char *bytes, std::size_t num)
+  {
     static_assert((std::numeric_limits<size_t>::max)() ==
                       (std::numeric_limits<std::size_t>::max)(),
                   "Incompatible size_t");
     EVP_DigestUpdate(ctx.get(), bytes, num);
   }
-  std::array<unsigned char, 32> digest() {
+  std::array<unsigned char, 32> digest()
+  {
     std::array<unsigned char, 32> tmp;
     EVP_DigestFinal_ex(ctx.get(), tmp.data(), nullptr);
     return tmp;
@@ -106,55 +120,67 @@ struct sha256_openssl {
 
 static std::unique_ptr<EVP_MD, openssl_md_destroyer> global_md;
 
-struct sha256_openssl_global {
+struct sha256_openssl_global
+{
   std::unique_ptr<EVP_MD_CTX, openssl_evp_destroyer> ctx;
 
-  sha256_openssl_global() : ctx(EVP_MD_CTX_create()) {
+  sha256_openssl_global() : ctx(EVP_MD_CTX_create())
+  {
     assert(static_cast<bool>(ctx));
     EVP_DigestInit_ex(ctx.get(), global_md.get(), NULL);
   }
 
-  void add_bytes(const unsigned char *bytes, std::size_t num) {
+  void add_bytes(const unsigned char *bytes, std::size_t num)
+  {
     static_assert((std::numeric_limits<size_t>::max)() ==
                       (std::numeric_limits<std::size_t>::max)(),
                   "Incompatible size_t");
     EVP_DigestUpdate(ctx.get(), bytes, num);
   }
-  std::array<unsigned char, 32> digest() {
+  std::array<unsigned char, 32> digest()
+  {
     std::array<unsigned char, 32> tmp;
     EVP_DigestFinal_ex(ctx.get(), tmp.data(), nullptr);
     return tmp;
   }
 };
 
-struct sha256_openssl_oneshot {
+struct sha256_openssl_oneshot
+{
   std::array<unsigned char, 32> digest_data;
-  void add_bytes(const unsigned char *bytes, std::size_t num) {
+  void add_bytes(const unsigned char *bytes, std::size_t num)
+  {
     SHA256(bytes, num, digest_data.data());
   }
-  std::array<unsigned char, 32> digest() {
+  std::array<unsigned char, 32> digest()
+  {
     return digest_data;
   }
 };
 
 #ifdef _WIN32
-struct bcrypt_alg_destroyer {
-  void operator()(BCRYPT_ALG_HANDLE ctx) const {
+struct bcrypt_alg_destroyer
+{
+  void operator()(BCRYPT_ALG_HANDLE ctx) const
+  {
     BCryptCloseAlgorithmProvider(ctx, 0);
   }
 };
 
-struct bcrypt_hash_destroyer {
+struct bcrypt_hash_destroyer
+{
   void operator()(BCRYPT_HASH_HANDLE ctx) const { BCryptDestroyHash(ctx); }
 };
 
-struct sha256_bcrypt {
+struct sha256_bcrypt
+{
   std::unique_ptr<void, bcrypt_alg_destroyer> alg;
   std::unique_ptr<void, bcrypt_hash_destroyer> ctx;
 
   std::array<unsigned char, 32> sha256_memory;
 
-  sha256_bcrypt() {
+  sha256_bcrypt()
+  {
     {
       BCRYPT_ALG_HANDLE handle;
       auto ret = BCryptOpenAlgorithmProvider(&handle, BCRYPT_SHA256_ALGORITHM,
@@ -172,17 +198,20 @@ struct sha256_bcrypt {
     }
   }
 
-  void add_bytes(const unsigned char *bytes, std::size_t num) {
+  void add_bytes(const unsigned char *bytes, std::size_t num)
+  {
     constexpr std::size_t max_bytes = (std::numeric_limits<ULONG>::max)();
     unsigned int size = 0;
-    for (std::size_t i = 0; i < num; i += size, bytes += size) {
+    for (std::size_t i = 0; i < num; i += size, bytes += size)
+    {
       size = static_cast<ULONG>((std::min)(max_bytes, num - i));
-      auto ret = BCryptHashData(ctx.get(), const_cast<unsigned char*>(bytes), size, 0);
+      auto ret = BCryptHashData(ctx.get(), const_cast<unsigned char *>(bytes), size, 0);
       (void)ret;
       assert(ret == STATUS_SUCCESS);
     }
   }
-  std::array<unsigned char, 32> digest() {
+  std::array<unsigned char, 32> digest()
+  {
     std::array<unsigned char, 32> tmp;
     auto ret = BCryptFinishHash(ctx.get(), tmp.data(), tmp.size(), 0);
     (void)ret;
@@ -193,13 +222,16 @@ struct sha256_bcrypt {
 #endif
 
 #ifdef BITCOIN_IMPL
-struct sha256_bitcoin {
+struct sha256_bitcoin
+{
   CSHA256 ctx;
 
-  void add_bytes(const unsigned char *bytes, std::size_t num) {
+  void add_bytes(const unsigned char *bytes, std::size_t num)
+  {
     ctx.Write(bytes, num);
   }
-  std::array<unsigned char, 32> digest() {
+  std::array<unsigned char, 32> digest()
+  {
     std::array<unsigned char, 32> tmp;
     ctx.Finalize(tmp.data());
     return tmp;
--- main.cpp
+++ main.cpp
@@ -3,69 +3,110 @@
 #include "algorithm_wrappers.h"
 
 #include <array>
-#include <atomic>
-#include <condition_variable>
 #include <cstring>
-#include <mutex>
 #include <random>
 #include <type_traits>
-
 #include <iostream>
-#include <syncstream>
+
+#include <hwloc.h>
+
+static int getPhysicalCores()
+{
+  static const int num_physical_cores = []()
+  {
+    int num_physical_cores = 8;
+    int res = 0;
+    hwloc_topology_t topology;
+    if (hwloc_topology_init(&topology) != 0)
+    {
+      goto cleanup;
+    }
+    if (hwloc_topology_load(topology) != 0)
+    {
+      goto cleanup;
+    }
+    res = hwloc_get_nbobjs_by_type(topology, hwloc_obj_type_t::HWLOC_OBJ_CORE);
+    if (res > 0)
+    {
+      num_physical_cores = res;
+    }
+  cleanup:
+    hwloc_topology_destroy(topology);
+    if (res <= 0)
+    {
+      std::cerr << "Could not determine number of physical cores\n";
+    }
+    return num_physical_cores;
+  }();
+
+  return num_physical_cores;
+}
 
 template <typename sha256_wrapper>
-class data_fixture : public benchmark::Fixture {
+class data_fixture : public benchmark::Fixture
+{
 public:
-  std::vector<unsigned char> data;
+  static thread_local std::vector<unsigned char> data;
 
   using sha256_wrapper_t = sha256_wrapper;
 
-  void SetUp(::benchmark::State &state) {
-    if (state.thread_index() == 0) {
+  void SetUp(::benchmark::State &state)
+  {
+    if (state.thread_index() == 0)
+    {
       global_md.reset(EVP_MD_fetch(NULL, "SHA256", NULL));
 #ifdef BITCOIN_IMPL
-      if constexpr (std::is_same_v<sha256_wrapper, sha256_bitcoin>) {
+      if constexpr (std::is_same_v<sha256_wrapper, sha256_bitcoin>)
+      {
         SHA256AutoDetect(sha256_implementation::USE_ALL);
       }
 #endif // BITCOIN_IMPL
-      // Fill data with reproducibly random bytes
-      std::mt19937_64 gen;
-      std::uint64_t size = static_cast<std::uint64_t>(state.range(0));
-      assert(size % sizeof(std::uint64_t) == 0);
-      data.reserve(size);
-      for (std::uint64_t i = 0; i < size; i += sizeof(std::uint64_t)) {
-        auto rnd = gen();
-        std::array<unsigned char, sizeof(std::uint64_t)> tmp;
-        std::memcpy(tmp.data(), &rnd, sizeof(std::uint64_t));
-        data.insert(data.end(), tmp.begin(), tmp.end());
-      }
+    }
+    // Fill data with reproducibly random bytes
+    std::mt19937_64 gen;
+    std::uint64_t size = static_cast<std::uint64_t>(state.range(0));
+    assert(size % sizeof(std::uint64_t) == 0);
+    data.reserve(size);
+    for (std::uint64_t i = 0; i < size; i += sizeof(std::uint64_t))
+    {
+      auto rnd = gen();
+      std::array<unsigned char, sizeof(std::uint64_t)> tmp;
+      std::memcpy(tmp.data(), &rnd, sizeof(std::uint64_t));
+      data.insert(data.end(), tmp.begin(), tmp.end());
     }
   }
-  void TearDown(::benchmark::State &state) {
+  void TearDown(::benchmark::State &state)
+  {
     if (state.thread_index() != 0)
       return;
     data.clear();
   }
 };
 
-#define BENCHMARK_SHA256(SHA256_TYPE)                                          \
-  BENCHMARK_TEMPLATE_DEFINE_F(data_fixture, BM_##SHA256_TYPE, SHA256_TYPE)     \
-  (::benchmark::State & state) {                                               \
-    /* TODO: Each thread should get its own NUMA-local copy of the data */     \
-    for (auto _ : state) {                                                     \
-      SHA256_TYPE sha256_obj;                                                  \
-      sha256_obj.add_bytes(data.data(), data.size());                          \
-      auto result = sha256_obj.digest();                                       \
-      benchmark::DoNotOptimize(result);                                        \
-      benchmark::ClobberMemory();                                              \
-    }                                                                          \
-    state.SetBytesProcessed(int64_t(state.iterations()) *                      \
-                            int64_t(state.range(0)) * state.threads());        \
-  }                                                                            \
-  BENCHMARK_REGISTER_F(data_fixture, BM_##SHA256_TYPE)                         \
-      ->Range(1LL << 8, 1LL << 16)                                             \
-      ->ThreadRange(1, 8)                                                      \
-      ->UseRealTime()                                                          \
+template <typename sha256_wrapper>
+thread_local std::vector<unsigned char> data_fixture<sha256_wrapper>::data;
+
+#define BENCHMARK_SHA256(SHA256_TYPE)                                                      \
+  BENCHMARK_TEMPLATE_DEFINE_F(data_fixture, BM_##SHA256_TYPE, SHA256_TYPE)                 \
+  (::benchmark::State & state)                                                             \
+  {                                                                                        \
+    for (auto _ : state)                                                                   \
+    {                                                                                      \
+      SHA256_TYPE sha256_obj;                                                              \
+      sha256_obj.add_bytes(data.data(), data.size());                                      \
+      auto result = sha256_obj.digest();                                                   \
+      benchmark::DoNotOptimize(result);                                                    \
+      benchmark::ClobberMemory();                                                          \
+    }                                                                                      \
+    /* TODO: Check why multiplying with state.threads() became necessary. */               \
+    /* https://github.com/google/benchmark/blob/main/docs/user_guide.md#custom-counters */ \
+    state.SetBytesProcessed(int64_t(state.iterations()) *                                  \
+                            int64_t(state.range(0)) * state.threads());                    \
+  }                                                                                        \
+  BENCHMARK_REGISTER_F(data_fixture, BM_##SHA256_TYPE)                                     \
+      ->Range(1LL << 8, 1LL << 16)                                                         \
+      ->ThreadRange(1, getPhysicalCores())                                                 \
+      ->UseRealTime()                                                                      \
       ->Name(#SHA256_TYPE);
 
 // BENCHMARK_SHA256(sha256_dummy); // Do nothing
